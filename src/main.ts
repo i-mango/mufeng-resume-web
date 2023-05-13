c6075a79636f15bf4a55a8016f30f8bd8f512be1
潘纪婷 <mango@imufeng.cn>
2023-05-14 01:41:55 +0800

    新增依赖并全局配置

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,20 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+// element plus
+import ElementPlus from "element-plus";
+// element icons
+import * as Icons from "@element-plus/icons-vue";
+// element css
+// import "element-plus/dist/index.css";
 
 const app = createApp(App)
-
+// 注册element Icons组件
+Object.keys(Icons).forEach(key => {
+    app.component(key, Icons[key as keyof typeof Icons]);
+});
 app.use(createPinia())
 app.use(router)
+app.use(ElementPlus)
 
 app.mount('#app')
