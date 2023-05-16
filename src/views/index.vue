202d664a13be5b2a585210b6edeb46144f72cab9
潘纪婷 <mango@imufeng.cn>
2023-05-16 17:35:00 +0800

    增加页面导航栏组件

---
+++ src/views/index.vue
@@ -0,0 +1,12 @@
+<script setup lang='ts'>
+import TopMenu from "@/components/topMenu.vue";
+const myName="Mu Feng"
+</script>
+
+<template>
+  <TopMenu :name="myName"/>
+</template>
+
+<style scoped>
+
+</style>
