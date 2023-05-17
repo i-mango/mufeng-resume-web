4ad9806863b336b1099887519f2f87f3cd0a68cb
潘纪婷 <mango@imufeng.cn>
2023-05-17 16:36:10 +0800

    增加首页布局

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,12 +1,53 @@
 <script setup lang='ts'>
-import TopMenu from "@/components/topMenu.vue";
-const myName="Mu Feng"
+import {ref} from "vue";
+import ownInfo from "../assets/ownInfo.json";
+
+const circleUrl = ref("")
+const {name, age, excpect_work, phoneNumber, email} = ownInfo
+// 获取浏览器可视区域宽高（兼容性比较好，不包括工具栏和滚动条）
+// const browserWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+// const browserHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
 </script>
 
 <template>
-  <TopMenu :name="myName"/>
+  <div class="content">
+    <!--    左侧-->
+    <div class="content-left">
+      <el-aside class="aside">
+        <el-avatar :size="50" :src="circleUrl"/>
+        <ul>
+          <li>姓名：{{ name }}</li>
+          <li>年龄：{{ age }}</li>
+          <li>职业：{{ excpect_work }}</li>
+          <li>手机号：{{ phoneNumber }}</li>
+          <li>邮箱：{{ email }}</li>
+        </ul>
+      </el-aside>
+    </div>
+    <!--    右侧-->
+    <div class="content-right">
+      <el-tabs>
+        <el-tab-pane label="我的信息"></el-tab-pane>
+        <el-tab-pane label="项目经验"></el-tab-pane>
+        <el-tab-pane label="工作经验"></el-tab-pane>
+        <el-tab-pane label="联系方式"></el-tab-pane>
+      </el-tabs>
+    </div>
+  </div>
 </template>
 
 <style scoped>
+.content{
+  display: flex;
+}
+.content-left{
+  float: left;
+}
+.aside {
+  background: #b4dcef;
+}
 
+.content-right {
+  float: left;
+}
 </style>
