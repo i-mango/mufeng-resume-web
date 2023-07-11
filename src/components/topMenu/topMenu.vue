aec40d7ca6c5fd2e94239f70ee042eb10d1824fd
潘纪婷 <mango@imufeng.cn>
2023-07-11 09:39:48 +0800

    修改topMenu

--- src/components/topMenu/topMenu.vue
+++ src/components/topMenu/topMenu.vue
@@ -1,20 +1,33 @@
-<script setup lang='ts'>
-import ownInfo from "@/assets/ownInfo.json";
+<script>
+import {ref, nextTick} from 'vue';
+import ownInfo from '@/assets/ownInfo.json'
+export default {
+  name: 'topMenu',
+  data () {
+    return {
+      activeIndex: '1',
+      targetEle: null,
+      own: ownInfo.own
+    }
+  },
+  methods: {
+    // 点击菜单栏的选择，自动滚动到对应的视图
+    async handleSelect (index) {
+      let name = ''
+      if (index === '1') name = 'homepage'
+      if (index === '4') name = 'word-exp'
+      if (index === '3') name = 'skills'
+      if (index === '2') name = 'profile'
+      if (index === '5') name = 'project-exp'
+      await nextTick(); // Wait for the next DOM update
 
-const {own} = ownInfo;
-const activeIndex = "1";
-
-// 点击菜单栏的选择，自动滚动到对应的视图
-const handleSelect = (index) => {
-  let name = ''
-  if (index === '1') name = 'homepage'
-  if (index === '4') name = 'word-exp'
-  if (index === '3') name = 'skills'
-  if (index === '2') name = 'profile'
-  const targetEle = document.querySelector('.' + name)
-  targetEle.scrollIntoView({ behavior: 'smooth' })
-
-};
+      const targetEle = document.querySelector('.' + name);
+      this.targetEle = ref(targetEle);
+      const offsetTop = this.targetEle.value.offsetTop;
+      document.documentElement.scrollTop = offsetTop - 150;
+    }
+  }
+}
 </script>
 
 <template>
@@ -43,10 +56,16 @@ const handleSelect = (index) => {
   width: 100%;
   top: 0;
   z-index: 100;
-  margin-left: -50px;
+  margin-left: -4%;
+}
+.el-menu--horizontal{
+  height: 100px;
+}
+.el-menu--horizontal > .el-menu-item {
+  font-size: 25px;
 }
 .logo-img{
-  width: 40px;
+  width: 60px;
   outline:none;
   vertical-align: middle;
   padding: 5px;
